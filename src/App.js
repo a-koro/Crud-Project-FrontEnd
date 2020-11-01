9b181864705cfe2784da07bc9750a7ffd02d25e5
a-koro <korove@hotmail.com>
2020-11-01 20:21:23 +0200

    new compoentss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
+import Article from './components/Article';
+import Author from './components/Author';
 
 function App() {
+
+  function fetchFromGetArticles() {
+    fetch("/api/getArticles")
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid p-0">
+      <BrowserRouter>
+        <Navbar/>
+        <button onClick={fetchFromGetArticles}>Get Articles</button>
+        <Switch>
+          <Route exact path="/" component={Article}/>
+          <Route path="/articles" component={Author}/>
+          <Route path="/admin"/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
