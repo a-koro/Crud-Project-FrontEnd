04f82e79ccd522ea6d233cd9af122f76ad276766
a-koro <korove@hotmail.com>
2020-12-18 18:29:27 +0200

    heroku test

--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Axios from 'axios';
-console.log("This is the base url" + process.env.BASE_URL);
-if(process.env.BASE_URL) {
-  Axios.defaults.baseURL = process.env.BASE_URL;
-}
+Axios.defaults.baseURL = 'https://mern-articlomaric-app.herokuapp.com';
 
 ReactDOM.render(
   <React.StrictMode>
