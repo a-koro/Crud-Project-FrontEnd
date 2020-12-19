e54c1c5514be610c986d5a9b760ddffaac2e42ce
a-koro <korove@hotmail.com>
2020-12-19 12:15:11 +0200

    minor changes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import Axios from 'axios';
 require('dotenv').config();
 
+console.log(process.env);
 if(process.env.NODE_ENV == "production") {
   Axios.defaults.baseURL = process.env.BASE_URL;
 }
